5ff30ca15a406afe412d411a45af61b9bb38667d
Adelina <59620862+adelina15@users.noreply.github.com>
2021-09-24 19:07:35 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/greeter/MainActivity.kt
+++ app/src/main/java/com/example/greeter/MainActivity.kt
@@ -2,12 +2,18 @@ package com.example.greeter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.google.android.material.internal.NavigationMenu
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var name: String
+    private lateinit var greetingTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,14 +21,26 @@ class MainActivity : AppCompatActivity() {
         val nameEditText = findViewById<EditText>(R.id.name_edit)
 
         button.setOnClickListener {
-            val name = nameEditText.text.toString()
-            if (name.isBlank()){
+            name = nameEditText.text.toString()
+            if (name.isBlank()) {
                 Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
-            }
-            else {
-                val greetingTextView = findViewById<TextView>(R.id.greeting_text)
+            } else {
+                greetingTextView = findViewById(R.id.greeting_text)
                 greetingTextView.text = "Hello, $name"
             }
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString("username", name)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        name = savedInstanceState.getString("username", "")
+        greetingTextView = findViewById(R.id.greeting_text)
+        greetingTextView.text = "Hello, $name"
+        super.onRestoreInstanceState(savedInstanceState)
+
     }
 }
